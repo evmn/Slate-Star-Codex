b590e0ae2cdabf226d69576c5a1e1ed461607959
evmn <38780274+evmn@users.noreply.github.com>
2022-01-21 14:46:59 +0800

    add 'SSC' to remove patterns, set more metadata in cmd

--- ebook.sh
+++ ebook.sh
@@ -3,8 +3,10 @@
 ebook-convert "ssc.recipe" .mobi \
         --authors="Scott Alexander" \
         --title="Slate Star Codex" \
+        --comments="A blog about science, medicine, philosophy, politics, and futurism." \
+        --rating=5 \
         --pubdate="2022-1-18" \
-		--cover="images/cover.jpg" \
+        --cover="images/cover.jpeg" \
         --output-profile=kindle_pw3 \
         --mobi-file-type=new \
         -vv --test
