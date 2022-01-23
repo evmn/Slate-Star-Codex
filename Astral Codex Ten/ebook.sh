a5d264f3e7a483e7a6ea17c8c4181fc5944aa0ae
evmn <38780274+evmn@users.noreply.github.com>
2022-01-23 22:51:41 +0800

    copy posts.json to /tmp/ before using it in recipe, add date to articles feed

--- Astral Codex Ten/ebook.sh
+++ Astral Codex Ten/ebook.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+cp  posts.json /tmp/posts.json
 
 ebook-convert "Astral Codex Ten.recipe" .mobi \
         --authors="Scott Alexander" \
         --title="Astral Codex Ten" \
         --pubdate="2022-1-18" \
-		--cover="cover.jpg" \
+        --cover="images/cover.jpg" \
         --output-profile=kindle_pw3 \
         --mobi-file-type=new \
-        -vv
+        -vv --test
